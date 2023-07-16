9e908a035adf71722064e9b3a26ea57b3ebe5136
Vaibhavbv <74303416+Vaibhavbv@users.noreply.github.com>
2023-07-16 23:17:19 +0530

    Update Sorted&Rotated.java

--- Arrays/Sorted&Rotated.java
+++ Arrays/Sorted&Rotated.java
@@ -1,16 +1,19 @@
 class Solution {
-    public boolean check(int[] nums) {
-        int count = 0;
-        int n = nums.length;
-        for(int i=0; i<n; i++){
-            if(nums[i]>nums[(i+1) % n]){
-                count++;
+    public int maxProfit(int[] prices) {
+        int bp = Integer.MAX_VALUE;
+        int mp = 0;
+        for(int i=0; i<prices.length; i++){
+            //check the minimum bp by comparing it to current price
+            if(bp<prices[i]){
+                int profit = prices[i] - bp;
+                //to store the maximum profit
+                mp = Math.max(mp,profit);
             }
-            if(count>1){
-                return false;
+            else{
+                //here we update the bp , if currprice <<< than bp , then this
+                bp = prices[i];
             }
         }
-        return true;
+        return mp;
     }
-    
 }

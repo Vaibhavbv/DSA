046ee87d1e5a0d6354f2e84726a1e0c3ca7fd648
Vaibhavbv <74303416+Vaibhavbv@users.noreply.github.com>
2023-07-16 23:18:07 +0530

    Update Best Time to Buy and Sell Stock.java

--- Arrays/Best Time to Buy and Sell Stock.java
+++ Arrays/Best Time to Buy and Sell Stock.java
@@ -3,11 +3,14 @@ class Solution {
         int bp = Integer.MAX_VALUE;
         int mp = 0;
         for(int i=0; i<prices.length; i++){
+            //check the minimum bp by comparing it to current price
             if(bp<prices[i]){
                 int profit = prices[i] - bp;
+                //to store the maximum profit
                 mp = Math.max(mp,profit);
             }
             else{
+                //here we update the bp , if currprice <<< than bp , then this
                 bp = prices[i];
             }
         }
